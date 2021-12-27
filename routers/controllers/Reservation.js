148c93189a8ef1c0db4805aa37cbf8b94e4a3a38
AmoorhAlshammary <Amirah.Sair.rae.js@tuwaiq.edu.sa>
2021-12-28 00:27:27 +0300

    backend

--- routers/controllers/Reservation.js
+++ routers/controllers/Reservation.js
@@ -42,7 +42,7 @@ const addReservation = async (req, res) => {
     // console.log(foundUser) // {_id: '2kj3h432kl5523h', name:'Amirah', username:'am', email:'a@a.com', password:'lskjdfklh2k3h423k4h23876823dsfk', isAdmin:false, isActive: true}
     // console.log(foundDecoration) // {_id: '2kj3h432kl5523hsdfdsfw', name:'Kitchen', description:'this is description', price:234, img:'https://image.com' }
     if(!foundUser || !foundDecoration){
-        return res.status(404).json({msg: 'there is no user or decoration with that id'})
+        return res.status(404).json({msg: 'there is no user or decoration '})
     }
     const newReservation = new ReservationModel({user:foundUser.id, decoration:foundDecoration.id, date});
     const response = await newReservation.save();
@@ -53,25 +53,25 @@ const addReservation = async (req, res) => {
   }
 };
 
-const updateReservation = async (req, res) => {
-  // console.log(req.body)
-  const { reservationId, userId, decorationId, date} = req.body;
-  try {
-    const foundReservation = await ReservationModel.findById(reservationId);
-    const foundUser = await UserModel.findById(userId);
-    const foundDecoration = await DecorationModel.findById(decorationId);
-    // console.log(foundUser) // {_id: '2kj3h432kl5523h', name:'Amirah', username:'am', email:'a@a.com', password:'lskjdfklh2k3h423k4h23876823dsfk', isAdmin:false, isActive: true}
-    // console.log(foundDecoration) // {_id: '2kj3h432kl5523hsdfdsfw', name:'Kitchen', description:'this is description', price:234, img:'https://image.com' }
-    if(!foundReservation || !foundUser || !foundDecoration){
-        return res.status(404).json({msg: 'there is no reservation, user or decoration with that id'});
-    }
-    const response = await ReservationModel.findByIdAndUpdate(foundReservation.id,{user:foundUser.id, decoration: foundDecoration.id, date},{new:true})
-    // console.log(response)
-    res.status(201).json(response);
-  } catch (error) {
-    res.send(error);
-  }
-};
+// const updateReservation = async (req, res) => {
+//   // console.log(req.body)
+//   const { reservationId, userId, decorationId, date} = req.body;
+//   try {
+//     const foundReservation = await ReservationModel.findById(reservationId);
+//     const foundUser = await UserModel.findById(userId);
+//     const foundDecoration = await DecorationModel.findById(decorationId);
+//     // console.log(foundUser) // {_id: '2kj3h432kl5523h', name:'Amirah', username:'am', email:'a@a.com', password:'lskjdfklh2k3h423k4h23876823dsfk', isAdmin:false, isActive: true}
+//     // console.log(foundDecoration) // {_id: '2kj3h432kl5523hsdfdsfw', name:'Kitchen', description:'this is description', price:234, img:'https://image.com' }
+//     if(!foundReservation || !foundUser || !foundDecoration){
+//         return res.status(404).json({msg: 'there is no reservation, user or decoration '});
+//     }
+//     const response = await ReservationModel.findByIdAndUpdate(foundReservation.id,{user:foundUser.id, decoration: foundDecoration.id, date},{new:true})
+//     // console.log(response)
+//     res.status(201).json(response);
+//   } catch (error) {
+//     res.send(error);
+//   }
+// };
 
 const deleteReservation = async (req, res)=>{
   const {id} = req.params;
--- routers/middlewares/Authorization.js
+++ routers/middlewares/Authorization.js
@@ -1,29 +1,28 @@
-const jwt = require("jsonwebtoken");
+//   require("jsonwebtoken");
 
-const UserModel = require('../../db/models/UserModel')
+// const UserModel = require('../../db/models/UserModel')
 
-const adminAuthorization = async (req, res, next) => {
-  try {
-    // we passed the user from the authentication through the req object
-    const userData = req.user;
-    try {
-      const foundUser = await UserModel.findById(userData.userId);
-      if(!foundUser){
-        return res.status(403).json({error: 'no user found'});
-      }
-      if(!foundUser.isAdmin){
-        return res.status(403).json({error: 'you do not have permission'});
-      }
-      next()
-    } catch (error) {
-      console.log(error)
-      return res.status(500).json({error: 'server error'}) 
-    }
-  } catch (error) {
-    console.log(error)
-    return res.status(403).json({error: 'no token found'});
-    // res.send(JSON.stringify(error));
-  }
-};
+// const adminAuthorization = async (req, res, next) => {
+//   try {
+//     const userData = req.user;
+//     try {
+//       const foundUser = await UserModel.findById(userData.userId);
+//       if(!foundUser){
+//         return res.status(403).json({error: 'no user found'});
+//       }
+//       if(!foundUser.isAdmin){
+//         return res.status(403).json({error: 'you do not have permission'});
+//       }
+//       next()
+//     } catch (error) {
+//       console.log(error)
+//       return res.status(500).json({error: 'server error'}) 
+//     }
+//   } catch (error) {
+//     console.log(error)
+//     return res.status(403).json({error: 'no token found'});
+//     // res.send(JSON.stringify(error));
+//   }
+// };
 
-module.exports = { adminAuthorization };
+// module.exports = { adminAuthorization };
