5398f284812388f0adce45817515c0e8a01e7f22
AmoorhAlshammary <Amirah.Sair.rae.js@tuwaiq.edu.sa>
2021-12-29 14:33:56 +0300

    backend

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/decoration").then(
+mongoose.connect("mongodb+srv://amirah:123@cluster0.kqgcx.mongodb.net/Decoration?retryWrites=true&w=majority").then(
   () => {
     console.log("DB connected");
   },
